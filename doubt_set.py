0f801d57c3452cdb583819248c3954a73efa427a
Vibek Sarkar <vibekananda7895@gmail.com>
2026-08-04 15:11:15 +0530

    doubt clear of last day

---
+++ doubt_set.py
@@ -0,0 +1,26 @@
+# class Prac:
+#     def he(self, name):
+#         print(f"hello {name}")
+
+# sd = Prac()
+# sd.he("ruth")
+
+class Circle:
+    def area(self, radius):
+        print(f"the area of circle is: {3.14 * radius * radius}")
+
+class Square:
+    def area(self, squa):
+        print(f"the side of square is: {squa * squa}")
+
+class Rectangle:
+    def area(self, length, width):
+        print(f"the area of rectange is: {length * width}")
+
+cir = Circle()
+cir.area(2)
+sq = Square()
+sq.area(2)
+rc = Rectangle()
+rc.area(10, 15)
+
